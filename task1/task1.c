aaf19261c3d8c0bd263acd6728baf9c71d934645
stanislav <stanislav@DESKTOP-3B6FBBT>
2025-09-23 01:29:02 +0300

    Добавьте файлы проекта.

---
+++ task1/task1.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	int num1, num2;
+
+	puts("Введите число 1:");
+	scanf("%d", &num1);
+	printf("Введено число 1: %d\n", num1);
+
+	puts("Введите число 2:");
+	scanf("%d", &num2);
+	printf("Введено число 2: %d\n", num2);
+
+	printf("Сумма: %d\n", num1 + num2);
+	printf("Разность: %d\n", num1 - num2);
+	printf("Произведение: %d\n", num1 * num2);
+	printf("Частное: %d\n", num1 / num2);
+	printf("Остаток от деления: %d\n", num1 % num2);
+}
---
+++ task2/task2.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+#define      D       2.54
+#define      P       2.32
+
+int main()
+{
+	int dym;
+	float result_d, result_p;
+
+	setlocale(LC_CTYPE, "RUS");
+	
+	puts("Введите дюймы:");
+	scanf("%d", &dym);
+
+	result_d = D * dym;
+	result_p = P * dym;
+
+	printf("%d английских дюймов – это %.2f см\n", dym, result_d);
+	printf("%d испанских дюймов – это %.2f см", dym, result_p);
+
+	return 0;
+}
